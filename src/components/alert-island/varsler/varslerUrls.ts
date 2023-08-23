193a3004acf672fc9df51f3fa0d700b2a84b1ae5
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2023-08-23 14:34:31 +0200

    Fiks url

--- src/components/alert-island/varsler/varslerUrls.ts
+++ src/components/alert-island/varsler/varslerUrls.ts
@@ -3,7 +3,7 @@ import { getEnvironment } from "../../../utils/environment";
 const VARSLER_URL = {
   local: "http://localhost:3000/minside/varsler",
   dev: "https://www.intern.dev.nav.no/minside/varsler",
-  prod: "https://www.nav.no/tms-varsel-api/minside/varsler",
+  prod: "https://www.nav.no/minside/varsler",
 };
 
 export const varslerUrl = VARSLER_URL[getEnvironment()];
