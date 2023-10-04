c27b9185fc86a7718dfffe17810df0e271f24919
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2023-10-04 13:02:49 +0200

    Hent inn aia

---
+++ src/mikrofrontends/arbeidssoker/Aia.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ContentLoader from "../../components/loader/ContentLoader";
+import { aiaCdnUrl, aiaManifestUrl, arbeidssokerUrl } from "./urls";
+import { aiaEntry, bundle } from "../entrypoints";
+import { useManifest } from "../../hooks/useManifest";
+import ErrorBoundary from "../../components/error-boundary/ErrorBoundary";
+import useSWRImmutable from "swr/immutable";
+import { fetcher } from "../../utils/api.client.ts";
+
+const Aia = () => {
+  const { data: arbeidssoker, isLoading: isLoadingArbeidssoker } = useSWRImmutable({ path: arbeidssokerUrl }, fetcher);
+  const [manifest, isLoadingManifest] = useManifest(aiaManifestUrl);
+
+  if (isLoadingArbeidssoker) {
+    return null;
+  }
+
+  if (!arbeidssoker?.erArbeidssoker) {
+    return null;
+  }
+
+  if (isLoadingManifest) {
+    return null;
+  }
+
+
+  const ArbeidsflateForInnloggetArbeidssoker = React.lazy(() => import(`${aiaCdnUrl}/${manifest[aiaEntry][bundle]}`));
+
+  return (
+    <React.Suspense fallback={<ContentLoader />}>
+      <ErrorBoundary>
+        <ArbeidsflateForInnloggetArbeidssoker />
+      </ErrorBoundary>
+    </React.Suspense>
+  );
+};
+
+export default Aia;
--- src/mikrofrontends/arbeidssoker/urls.ts
+++ src/mikrofrontends/arbeidssoker/urls.ts
@@ -12,6 +12,12 @@ const AIA_CDN_URL = {
   prod: "https://cdn.nav.no/paw/aia",
 };
 
+const ARBEIDSSOKER_URL = {
+  local: "http://localhost:3000/er-arbeidssoker",
+  dev: "https://www.intern.dev.nav.no/tms-min-side-proxy/aia/aia-backend/er-arbeidssoker",
+  prod: "https://www.nav.no/tms-min-side-proxy/aia/aia-backend/er-arbeidssoker",
+};
 
 export const aiaCdnUrl = AIA_CDN_URL[getEnvironmentClientSide()];
 export const aiaManifestUrl = `${AIA_URL[getEnvironmentClientSide()]}/manifest.json`;
+export const arbeidssokerUrl = ARBEIDSSOKER_URL[getEnvironmentClientSide()];
