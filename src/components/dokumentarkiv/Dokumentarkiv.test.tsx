b313a5d775fc7687a8af03d74085c343a84eb4d3
Atle Nordland <atle.nordland@nav.no>
2024-01-04 10:44:21 +0100

    dokumentarkiv-tester

--- src/components/dokumentarkiv/Dokumentarkiv.test.tsx
+++ src/components/dokumentarkiv/Dokumentarkiv.test.tsx
@@ -1,13 +1,110 @@
 import { render, screen } from '@testing-library/react';
 import { expect, test } from 'vitest';
 import Dokumenter from './dokumenter/Dokumenter';
+import { server } from 'src/mocks/server';
+import { axe } from "vitest-axe";
+import { SWRConfig } from 'swr';
+import { HttpResponse, http } from 'msw';
+import { dokumentarkivUrl, mineSakerApiSisteUrl } from './dokumentarkivUrls';
 
 test("dokumentarkiv", async () => {
+  const { container } = render(
+    <SWRConfig value={{ provider: () => new Map() }}>
+      <Dokumenter language={"nb"}/>
+    </SWRConfig>
+  );
+  expect(await screen.findByRole("heading", { name: "Blabla" })).toBeInTheDocument()
+  expect(await axe(container)).toHaveNoViolations();
+});
 
-    render(
-        <Dokumenter language={"nb"}/>
-        );
+test("tomt panel lenker til dokumentarkiv", async () => {
+  server.use(
+    http.get(
+      mineSakerApiSisteUrl,
+      () => {
+        return HttpResponse.json({
+          sakstemaer: []
+        })
+      }
+    )
+  );
+  
+  const { container } = render(
+    <SWRConfig value={{ provider: () => new Map() }}>
+      <Dokumenter language={"nb"}/>
+    </SWRConfig>
+  );
+  expect(await screen.findByRole("link")).toHaveAttribute("href", dokumentarkivUrl);
+  expect(await axe(container)).toHaveNoViolations();
+});
 
-    expect(await screen.findByRole("heading", { name: "Blabla" })).toBeInTheDocument()
+test("viser siste dokument", async () => {
+  server.use(
+    http.get(
+      mineSakerApiSisteUrl,
+      () => {
+        return HttpResponse.json({
+          sakstemaer: [
+            {
+              navn: "Serviceklager",
+              kode: "SER",
+              sistEndret: "2023-05-25T12:41:02Z",
+              detaljvisningUrl: "https://localhost:3000/dokumenturl",
+            },
+          ],
+        })
+      }
+    )
+  );
+
+  const { container } = render(
+    <SWRConfig value={{ provider: () => new Map() }}>
+      <Dokumenter language={"nb"}/>
+    </SWRConfig>
+  );
+
+  expect(await screen.findByRole("heading", { name: "Serviceklager", level: 2 })).toBeInTheDocument();
+  expect(await screen.findByRole("link", { name: /Serviceklager/ })).toHaveAttribute(
+    "href",
+    "https://localhost:3000/dokumenturl"
+  );
+  expect(await screen.findByText("Sist endret: 25. mai 2023")).toBeInTheDocument();
+  expect(await screen.findByRole("link", { name: "Gå til dokumentarkivet" })).toHaveAttribute("href", dokumentarkivUrl);
+  expect(await axe(container)).toHaveNoViolations();
+});
+
+test("viser to siste dokumenter", async () => {
+  server.use(
+    http.get(
+      mineSakerApiSisteUrl,
+      () => {
+        return HttpResponse.json({
+          sakstemaer: [
+            {
+              navn: "Serviceklager",
+              kode: "SER",
+              sistEndret: "2023-05-25T12:41:02Z",
+              detaljvisningUrl: "https://localhost:3000/dokumenturl1",
+            },
+            {
+              navn: "Arbeidsavklaringspenger",
+              kode: "AAP",
+              sistEndret: "2023-04-20T11:33:51Z",
+              detaljvisningUrl: "https://localhost:3000/dokumenturl2",
+            },
+          ],
+        })
+      }
+    )
+  );
+
+  render(
+    <SWRConfig value={{ provider: () => new Map() }}>
+      <Dokumenter language={"nb"}/>
+    </SWRConfig>
+  );
+
+  expect(await screen.findByRole("heading", { name: "Serviceklager", level: 2 })).toBeInTheDocument();
+  expect(await screen.findByRole("heading", { name: "Arbeidsavklaringspenger", level: 2 })).toBeInTheDocument();
 });
   
--- vitest-setup.ts
+++ vitest-setup.ts
@@ -1,9 +1,14 @@
 import matchers from "@testing-library/jest-dom/matchers";
 import { afterAll, afterEach, beforeAll, expect } from "vitest";
-import { server } from "./src/mocks/server";
 import { cleanup } from "@testing-library/react";
+import * as axeMatchers from "vitest-axe/matchers";
+import { server } from "src/mocks/server";
 
 expect.extend(matchers)
+expect.extend(axeMatchers);
+
+// @ts-expect-error mock for å fikse jsdom-feil i testene
+HTMLCanvasElement.prototype.getContext = vi.fn();
 
 beforeAll(() => {
   server.listen({ onUnhandledRequest: "warn" });
