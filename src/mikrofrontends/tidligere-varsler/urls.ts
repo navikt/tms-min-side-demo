cc36100f5adc35516e1d5c81ddddb28a8f53889b
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2024-01-19 11:01:16 +0100

    Henter manifest og bundle fra ny url etter endring av repo-navn

--- src/mikrofrontends/tidligere-varsler/urls.ts
+++ src/mikrofrontends/tidligere-varsler/urls.ts
@@ -2,8 +2,8 @@ import { getEnvironmentClientSide } from "@utils/environment.client.ts";
 
 const TIDLIGERE_VARSLER_URL = {
   local: "http://localhost:3000",
-  dev: "https://www.intern.dev.nav.no/tms-min-side-varslinger",
-  prod: "https://www.nav.no/tms-min-side-varslinger",
+  dev: "https://www.intern.dev.nav.no/tms-tidligere-varsler-mikrofrontend",
+  prod: "https://www.nav.no/tms-tidligere-varsler-mikrofrontend",
 };
 
 export const tidligereVarslerUrl = TIDLIGERE_VARSLER_URL[getEnvironmentClientSide()];
