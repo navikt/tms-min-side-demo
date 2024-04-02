b9e9dc3e8116fea100f0365917d481ece9fb3dff
sasoria <sergio.alberto.arevalo.soria@nav.no>
2024-04-02 14:52:46 +0200

    Css tweaks
    Co-authored-by: Amirhossien Lotfi <amirhossien.lotfi@nav.no>
    Co-authored-by: rannveigskjerve <rannveig.alette.skjerve@nav.no>

--- src/components/aktuelt/Aktuelt.tsx
+++ src/components/aktuelt/Aktuelt.tsx
@@ -1,11 +1,12 @@
 import useSWRImmutable from "swr/immutable";
 import { fetcher } from "@utils/api.client.ts";
-import type { Language } from "@language/language.ts";
 import { microfrontendsUrl } from "@components/oversikt/urls.ts";
 import { BodyShort } from "@navikt/ds-react";
 import MicrofrontendWrapper from "@components/oversikt/MicrofrontendWrapper.tsx";
 import { EnabledMicrofrontend } from "@components/oversikt/microfrontendTypes.tsx";
-import {text} from "@language/aktuelt.ts"
+import type { Language } from "@language/language.ts";
+import { text } from "@language/aktuelt.ts"
+import style from "./Aktuelt.module.css"
 
 interface Props {
   language: Language;
@@ -16,11 +17,12 @@ const Aktuelt = ({ language }: Props) => {
 
   if (isLoading || data === null)
     return null;
+
   return (
-    <>
-      <BodyShort size={"medium"}>{text.aktuelt[language]}</BodyShort>
+    <div className={style.container}>
+      <BodyShort className={style["aktuelt"]} size={"medium"} textColor={"subtle"}>{text.aktuelt[language]}</BodyShort>
       {data?.aktuelt?.map((microfrontend : EnabledMicrofrontend) => <MicrofrontendWrapper key={microfrontend.microfrontend_id} manifestUrl={microfrontend.url}/>)}
-    </>
+    </div>
   );
 };
 
