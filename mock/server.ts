8580af3e83a2f2a6748c312f4cd4bfcf5240ecf6
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2024-04-12 10:57:12 +0200

    Fjern ubrukt mock

--- mock/server.ts
+++ mock/server.ts
@@ -1,23 +1,20 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { cors } from "hono/cors";
-import aiaManifest from "./data/aia-manifest.json" assert { type: "json" };
-import manifest from "./data/manifest.json" assert { type: "json" };
-import meldekortinfo from "./data/meldekortinfo.json" assert { type: "json" };
+import aiaManifest from "./data/microfrontend/aia-manifest.json" assert { type: "json" };
+import manifest from "./data/microfrontend/manifest.json" assert { type: "json" };
 import navn from "./data/navn.json" assert { type: "json" };
-import oppfolging from "./data/oppfolging.json" assert { type: "json" };
 import selector from "./data/selector.json" assert { type: "json" };
 import sisteSaker from "./data/siste-saker.json" assert { type: "json" };
 import sakstemaerEgne from "./data/sakstemaer-egne.json" assert { type: "json" };
 import varsler from "./data/varsler.json" assert { type: "json" };
 import utkast from "./data/utkast.json" assert { type: "json" };
-import utkastDigisos from "./data/utkast-digisos.json" assert { type: "json" };
 import utbetalinger from "./data/utbetalinger.json" assert { type: "json" };
 import innboks from "./data/innboks.json" assert { type: "json" };
 import erArbeidssoker from "./data/er-arbeidssoker.json" assert { type: "json" };
 import status from "./data/status.json" assert { type: "json" };
-import mikrofrontend from "./data/mikrofrontend.js";
-import { mikrofrontendBundle } from "./data/microfrontend-oversikt.ts";
+import mikrofrontend from "./data/microfrontend/mikrofrontend.js";
+import { mikrofrontendBundle } from "./data/microfrontend/microfrontend-oversikt.ts";
 
 const api = new Hono();
 
@@ -26,16 +23,8 @@ api.use("/*", cors({
   credentials: true,
 }));
 
-api.get('/meldekortinfo', (c) => {
-  return c.json(meldekortinfo);
-});
-
 api.get('/navn', (c) => {
   return c.json(navn);
-});
-
-api.get('/oppfolging', (c) => {
-  return c.json(oppfolging);
 });
 
 api.get('/selector/din-oversikt', (c) => {
@@ -50,16 +39,42 @@ api.get('/utkast', (c) => {
   return c.json(utkast);
 });
 
-api.get('/utkast-digisos', (c) => {
-  return c.json(utkastDigisos);
-});
-
 api.get('/utbetalinger/siste', (c) => {
   return c.json(utbetalinger);
 });
 
 api.get('/innboks', (c) => {
   return c.json(innboks);
+});
+
+
+
+api.get('/siste-saker', (c) => {
+  return c.json(sisteSaker);
+});
+
+api.get('/sakstemaer/egne', (c) => {
+  return c.json(sakstemaerEgne);
+});
+
+api.get('/er-arbeidssoker', (c) => {
+  return c.json(erArbeidssoker);
+});
+
+api.get('/login/status', (c) => {
+  return c.json(status);
+});
+
+api.post('/statistikk', (c) => {
+  return c.text("Done");
+});
+
+api.post('/collect', (c) => {
+  return c.text("Done")
+});
+
+api.get('/aia/manifest.json', (c) => {
+  return c.json(aiaManifest)
 });
 
 api.get('/manifest.json', (c) => {
@@ -106,40 +121,12 @@ api.get('/aia/bundle.js', (c) => {
   });
 });
 
-api.get('/aia/manifest.json', (c) => {
-  return c.json(aiaManifest)
-});
-
 api.get('/meldekort/bundle.js', (c) => {
   return new Response(mikrofrontendBundle("Meldekort", "5vh"),  {
     headers: {
       "Content-Type": "text/javascript",
     },
   });
-});
-
-api.get('/siste-saker', (c) => {
-  return c.json(sisteSaker);
-});
-
-api.get('/sakstemaer/egne', (c) => {
-  return c.json(sakstemaerEgne);
-});
-
-api.get('/er-arbeidssoker', (c) => {
-  return c.json(erArbeidssoker);
-});
-
-api.get('/login/status', (c) => {
-  return c.json(status);
-});
-
-api.post('/statistikk', (c) => {
-  return c.text("Done");
-});
-
-api.post('/collect', (c) => {
-  return c.text("Done")
 });
 
 serve(api);
