1846d5ec20db96c73c7bc639300439ebc3124e4e
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2024-04-22 11:14:31 +0200

    Nytt endepunkt for microfrontends

---
+++ src/pages/api/microfrontend/bundle.ts
@@ -0,0 +1,11 @@
+import type { APIRoute } from "astro";
+import { mikrofrontendBundle } from "../../../../mock/data/microfrontend/microfrontend-oversikt.ts";
+
+export const GET: APIRoute = async function get({ params, request }) {
+  return new Response(mikrofrontendBundle("Microfrontend", "5vh"), {
+    headers: {
+      "Content-Type": "text/javascript",
+    },
+    status: 200
+  });
+};
